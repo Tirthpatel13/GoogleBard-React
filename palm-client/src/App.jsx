1d7f7cdee61552682784f34ba1aa5f77c5aea552
Tirthpatel13 <tirthpatel131101@gmail.com>
2024-11-15 14:18:39 -0500

    new update

---
+++ palm-client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <main>
+      <h1>MyPrompter</h1>
+      <article>
+        <dev>
+          <textarea></textarea>
+          <button>Go</button>
+        </dev>
+      </article>
+    </main>
+  )
+}
+
+export default App
---
+++ palm-server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { TextServiceClient } =
+  require("@google-ai/generativelanguage").v1beta2;
+
+const { GoogleAuth } = require("google-auth-library");
+
+const MODEL_NAME = "models/text-bison-001";
+const API_KEY = process.env.API_KEY;
+
+const client = new TextServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+
+let answer = null;
+const prompt = "Repeat after me: one, two,";
+
+app.get('/api', (req, res) => {
+  client
+    .generateText({
+      model: MODEL_NAME,
+      prompt: {
+        text: prompt,
+      },
+    })
+    .then((result) => {
+      answer = result[0].candidates[0].output;
+      res.json(answer)
+    }).catch((err) => {
+      console.error(err.details);
+      res.json(err.details);
+    });
+});
+
+app.listen(3333, () => console.log('Server running on port 3333'));
